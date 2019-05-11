fc2cfdaca69c644b657c484483ac829f5bbeaf48
alisabudur <alisabudur@gmail.com>
2019-05-11 23:18:20 +0300

    Added output variables to Testing Page

--- SoftwareQualityPrediction/ViewModels/TestingViewModel.cs
+++ SoftwareQualityPrediction/ViewModels/TestingViewModel.cs
@@ -16,6 +16,7 @@ namespace SoftwareQualityPrediction.ViewModels
         {
             _uploadFileViewModel = new UploadFileViewModel {Mediator = this};
             _selectInputVariablesViewModel = new SelectItemsViewModel {Mediator = this};
+            _selectOutputVariablesViewModel = new SelectItemsViewModel {Mediator = this};
             _errorList = new Dictionary<string, string>();
             _neuralNetworkInputsCount = 1;
             _testingCompletedMessageVisibility = Visibility.Hidden;
@@ -46,6 +47,16 @@ namespace SoftwareQualityPrediction.ViewModels
             {
                 _selectInputVariablesViewModel = value;
                 OnPropertyChanged(nameof(SelectInputVariablesViewModel));
+            }
+        }
+
+        public SelectItemsViewModel SelectOutputVariablesViewModel
+        {
+            get { return _selectOutputVariablesViewModel; }
+            set
+            {
+                _selectOutputVariablesViewModel = value;
+                OnPropertyChanged(nameof(SelectOutputVariablesViewModel));
             }
         }
 
@@ -97,9 +108,11 @@ namespace SoftwareQualityPrediction.ViewModels
             if (colleague == _uploadFileViewModel)
             {
                 _selectInputVariablesViewModel.Receive(message);
+                _selectOutputVariablesViewModel.Receive(message);
             }
 
             OnPropertyChanged(nameof(SelectInputVariablesViewModel));
+            OnPropertyChanged(nameof(SelectOutputVariablesViewModel));
         }
 
         #endregion
@@ -142,6 +155,17 @@ namespace SoftwareQualityPrediction.ViewModels
 
                             break;
                         }
+                    case nameof(SelectOutputVariablesViewModel):
+                    {
+                        if (SelectOutputVariablesViewModel.UnselectedItems.Any() &&
+                            !SelectOutputVariablesViewModel.SelectedItems.Any())
+                        {
+                            error = string.Format(Properties.Resources.NoItemSelectedValidationMessage,
+                                Properties.Resources.OutputVariablesCaption);
+                        }
+
+                        break;
+                    }
                     case nameof(NeuralNetworkPath):
                     {
                         if (!File.Exists(_neuralNetworkPath))
@@ -154,6 +178,7 @@ namespace SoftwareQualityPrediction.ViewModels
                             error = string.Format(Properties.Resources.FieldIsRequiredValidationMessage,
                                 Properties.Resources.NeuralNetworkPathCaption);
                         }
+
                         break;
                     }
                 }
@@ -207,6 +232,7 @@ namespace SoftwareQualityPrediction.ViewModels
 
         private UploadFileViewModel _uploadFileViewModel;
         private SelectItemsViewModel _selectInputVariablesViewModel;
+        private SelectItemsViewModel _selectOutputVariablesViewModel;
         private string _neuralNetworkPath;
         private int _neuralNetworkInputsCount;
         private int _progressBarValue;
